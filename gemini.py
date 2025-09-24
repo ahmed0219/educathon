db5f9fce025d17e367a4b31c7b32cfdcfe379b47
Ahmed Selmi <ahmedselmi939@gmail.com>
2025-09-24 01:19:15 +0100

    'change'

--- gemini.py
+++ gemini.py
@@ -1,11 +1,11 @@
-import chromadb
+
 from dotenv import load_dotenv
 import google.generativeai as genai
 import streamlit as st
 
 import os
 load_dotenv()
-genai.configure(api_key=os.getenv("GEMINI_API_KEY"))
+genai.configure(api_key=st.secrets["GEMINI_API_KEY"])
 
 model = genai.GenerativeModel("models/gemini-2.0-flash")
 
